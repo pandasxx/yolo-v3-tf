75fb3b285b2783889d32c4eec9788e749a9efc4b
pandasxx <18512503190@163.com>
2018-07-20 22:33:26 +0800

    first version

---
+++ yolo-v3-tf/net/config.py
@@ -0,0 +1,49 @@
+from easydict import EasyDict as edict
+import numpy as np
+
+__C = edict()
+# Consumers can get config by:
+#   from config import cfg
+cfg = __C
+
+__C.anchors = np.array([[10, 13], [16, 30], [33, 23], [30, 61], [62, 45], [59, 119], [116, 90], [156, 198], [373, 326]])
+__C.classes = 20
+__C.num = 9
+__C.num_anchors_per_layer = 3
+__C.batch_size = 8
+__C.scratch = False
+__C.names = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+#
+# Training options
+#
+__C.train = edict()
+
+__C.train.ignore_thresh = .5
+__C.train.momentum = 0.9
+__C.train.decay = 0.0005
+__C.train.learning_rate = 0.001
+__C.train.max_batches = 50200
+__C.train.lr_steps = [40000, 45000]
+__C.train.lr_scales = [.1, .1]
+__C.train.max_truth = 30
+__C.train.mask = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+__C.train.image_resized = 384   # { 320, 352, ... , 608} multiples of 32
+__C.train.random = 0
+
+#
+# image process options
+#
+__C.preprocess = edict()
+__C.preprocess.angle = 0
+__C.preprocess.saturation = 1.5
+__C.preprocess.exposure = 1.5
+__C.preprocess.hue = .1
+__C.preprocess.jitter = .3
+__C.preprocess.random = 1
+
+#
+# image format
+#
+__C.data = edict()
+__C.data.img_size = [608, 608]
---
+++ yolo-v3-tf/net/yolo_top.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+from net.darknet53 import Darknet53
+from net.yolo_layer import yolo_head, yolo_det, preprocess_true_boxes, confidence_loss, cord_cls_loss
+from net.config import cfg
+
+
+class yolov3:
+
+    def __init__(self, img, truth, istraining, decay_bn=0.99):
+        self.img = img
+        self.truth = truth
+        self.istraining = istraining
+        self.decay_bn = decay_bn
+        self.img_shape = tf.shape(self.img)
+        with tf.variable_scope("Feature_Extractor"):
+            feature_extractor = Darknet53('darknet53.conv.74.npz', scratch=cfg.scratch)
+            self.feats52 = feature_extractor.build(self.img, self.istraining, self.decay_bn)
+        with tf.variable_scope("Head"):
+            head = yolo_head(self.istraining)
+            self.yolo123, self.yolo456, self.yolo789 = head.build(self.feats52,
+                                                                  feature_extractor.res18,
+                                                                  feature_extractor.res10)
+        with tf.variable_scope("Detection_0"):
+            self.anchors0 = tf.constant(cfg.anchors[cfg.train.mask[0]], dtype=tf.float32)
+            det = yolo_det(self.anchors0, cfg.classes, self.img_shape)
+            self.pred_xy0, self.pred_wh0, self.pred_confidence0, self.pred_class_prob0, self.loc_txywh0 = det.build(self.yolo123)
+        with tf.variable_scope("Detection_1"):
+            self.anchors1 = tf.constant(cfg.anchors[cfg.train.mask[1]], dtype=tf.float32)
+            det = yolo_det(self.anchors1, cfg.classes, self.img_shape)
+            self.pred_xy1, self.pred_wh1, self.pred_confidence1, self.pred_class_prob1, self.loc_txywh1 = det.build(self.yolo456)
+        with tf.variable_scope("Detection_2"):
+            self.anchors2 = tf.constant(cfg.anchors[cfg.train.mask[2]], dtype=tf.float32)
+            det = yolo_det(self.anchors2, cfg.classes, self.img_shape)
+            self.pred_xy2, self.pred_wh2, self.pred_confidence2, self.pred_class_prob2, self.loc_txywh2 = det.build(self.yolo789)
+
+
+    def compute_loss(self):
+        with tf.name_scope('Loss_0'):
+            matching_true_boxes, detectors_mask, loc_scale = preprocess_true_boxes(self.truth,
+                                                                                   self.anchors0,
+                                                                                   tf.shape(self.yolo123),
+                                                                                   self.img_shape)
+            objectness_loss = confidence_loss(self.pred_xy0, self.pred_wh0, self.pred_confidence0, self.truth, detectors_mask)
+            cord_loss = cord_cls_loss(detectors_mask, matching_true_boxes,
+                                      cfg.classes, self.pred_class_prob0, self.loc_txywh0, loc_scale)
+            loss1 = objectness_loss + cord_loss
+        with tf.name_scope('Loss_1'):
+            matching_true_boxes, detectors_mask, loc_scale = preprocess_true_boxes(self.truth,
+                                                                                   self.anchors1,
+                                                                                   tf.shape(self.yolo456),
+                                                                                   self.img_shape)
+            objectness_loss = confidence_loss(self.pred_xy1, self.pred_wh1, self.pred_confidence1, self.truth, detectors_mask)
+            cord_loss = cord_cls_loss(detectors_mask, matching_true_boxes,
+                                      cfg.classes, self.pred_class_prob1, self.loc_txywh1, loc_scale)
+            loss2 = objectness_loss + cord_loss
+        with tf.name_scope('Loss_2'):
+            matching_true_boxes, detectors_mask, loc_scale = preprocess_true_boxes(self.truth,
+                                                                                   self.anchors2,
+                                                                                   tf.shape(self.yolo789),
+                                                                                   self.img_shape)
+            objectness_loss = confidence_loss(self.pred_xy2, self.pred_wh2, self.pred_confidence2, self.truth, detectors_mask)
+            cord_loss = cord_cls_loss(detectors_mask, matching_true_boxes,
+                                      cfg.classes, self.pred_class_prob2, self.loc_txywh2, loc_scale)
+            loss3 = objectness_loss + cord_loss
+
+        self.loss = loss1 + loss2 + loss3
+        return self.loss
+
+    def pedict(self, img_hw, iou_threshold=0.5, score_threshold=0.5):
+        """
+        follow yad2k - yolo_eval
+        For now, only support single image prediction
+        :param iou_threshold:
+        :return:
+        """
+        img_hwhw = tf.expand_dims(tf.stack([img_hw[0], img_hw[1]] * 2, axis=0), axis=0)
+        with tf.name_scope('Predict_0'):
+            pred_loc0 = tf.concat([self.pred_xy0[..., 1:] - 0.5 * self.pred_wh0[..., 1:],
+                                   self.pred_xy0[..., 0:1] - 0.5 * self.pred_wh0[..., 0:1],
+                                   self.pred_xy0[..., 1:] + 0.5 * self.pred_wh0[..., 1:],
+                                   self.pred_xy0[..., 0:1] + 0.5 * self.pred_wh0[..., 0:1]
+                                   ], axis=-1)  # (y1, x1, y2, x2)
+            pred_loc0 = tf.maximum(tf.minimum(pred_loc0, 1), 0)
+            pred_loc0 = tf.reshape(pred_loc0, [-1, 4]) * img_hwhw
+            pred_obj0 = tf.reshape(self.pred_confidence0, shape=[-1])
+            pred_cls0 = tf.reshape(self.pred_class_prob0, [-1, cfg.classes])
+        with tf.name_scope('Predict_1'):
+            pred_loc1 = tf.concat([self.pred_xy1[..., 1:] - 0.5 * self.pred_wh1[..., 1:],
+                                   self.pred_xy1[..., 0:1] - 0.5 * self.pred_wh1[..., 0:1],
+                                   self.pred_xy1[..., 1:] + 0.5 * self.pred_wh1[..., 1:],
+                                   self.pred_xy1[..., 0:1] + 0.5 * self.pred_wh1[..., 0:1]
+                                   ], axis=-1)  # (y1, x1, y2, x2)
+            pred_loc1 = tf.maximum(tf.minimum(pred_loc1, 1), 0)
+            pred_loc1 = tf.reshape(pred_loc1, [-1, 4]) * img_hwhw
+            pred_obj1 = tf.reshape(self.pred_confidence1, shape=[-1])
+            pred_cls1 = tf.reshape(self.pred_class_prob1, [-1, cfg.classes])
+        with tf.name_scope('Predict_2'):
+            pred_loc2 = tf.concat([self.pred_xy2[..., 1:] - 0.5 * self.pred_wh2[..., 1:],
+                                   self.pred_xy2[..., 0:1] - 0.5 * self.pred_wh2[..., 0:1],
+                                   self.pred_xy2[..., 1:] + 0.5 * self.pred_wh2[..., 1:],
+                                   self.pred_xy2[..., 0:1] + 0.5 * self.pred_wh2[..., 0:1]
+                                   ], axis=-1)  # (y1, x1, y2, x2)
+            pred_loc2 = tf.maximum(tf.minimum(pred_loc2, 1), 0)
+            pred_loc2 = tf.reshape(pred_loc2, [-1, 4]) * img_hwhw
+            pred_obj2 = tf.reshape(self.pred_confidence2, shape=[-1])
+            pred_cls2 = tf.reshape(self.pred_class_prob2, [-1, cfg.classes])
+
+        self.pred_loc = tf.concat([pred_loc0, pred_loc1, pred_loc2], axis=0, name='pred_y1x1y2x2')
+        self.pred_obj = tf.concat([pred_obj0, pred_obj1, pred_obj2], axis=0, name='pred_objectness')
+        self.pred_cls = tf.concat([pred_cls0, pred_cls1, pred_cls2], axis=0, name='pred_clsprob')
+
+        # score filter
+        box_scores = tf.expand_dims(self.pred_obj, axis=1) * self.pred_cls      # (?, 20)
+        box_label = tf.argmax(box_scores, axis=-1)      # (?, )
+        box_scores_max = tf.reduce_max(box_scores, axis=-1)     # (?, )
+
+        pred_mask = box_scores_max > score_threshold
+        boxes = tf.boolean_mask(self.pred_loc, pred_mask)
+        scores = tf.boolean_mask(box_scores_max, pred_mask)
+        classes = tf.boolean_mask(box_label, pred_mask)
+
+        # non_max_suppression
+        idx_nms = tf.image.non_max_suppression(boxes, scores,
+                                               max_output_size=5,
+                                               iou_threshold=iou_threshold)
+        boxes = tf.gather(boxes, idx_nms)
+        scores = tf.gather(scores, idx_nms)
+        classes = tf.gather(classes, idx_nms)
+
+        return boxes, scores, classes
