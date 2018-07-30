06a6bba327f9b4a8326b0fa57dd85757a621f03d
pandasxx <18512503190@163.com>
2018-07-30 20:47:50 +0800

    realize basic train and predict

--- yolo-v3-tf/net/config.py
+++ yolo-v3-tf/net/config.py
@@ -25,11 +25,21 @@ __C.train.decay = 0.0005
 __C.train.learning_rate = 0.001
 __C.train.max_batches = 50200
 __C.train.lr_steps = [40000, 45000]
-__C.train.lr_scales = [.1, .1]
+__C.train.lr_scales = [1e-3, 1e-4, 1e-5]
 __C.train.max_truth = 30
 __C.train.mask = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
 __C.train.image_resized = 608   # { 320, 352, ... , 608} multiples of 32
 __C.train.random = 0
+
+#
+# Prediction options
+#
+__C.predict = edict()
+
+__C.predict.batch_size = 1
+__C.predict.scratch = True
+__C.predict.iou_thresh = 0.3
+__C.predict.score_thresh = 0.3
 
 #
 # image process options
@@ -56,4 +66,3 @@ __C.path = edict()
 __C.path.train_data_path = "enhancement01_608.tfrecords"
 __C.path.darknet_weights_path = "../darknet53.conv.74.npz"
 __C.path.ckpt_dir = './ckpt/'
-
